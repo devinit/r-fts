ac8a874f021fb61ac60519c8ecbdd42bd65a2db3
Alex Miller <alex.k.miller@gmail.com>
2019-03-22 14:51:54 -0400

    All flows

---
+++ all_flows.R
@@ -0,0 +1,107 @@
+list.of.packages <- c("data.table","httr","reshape")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only=T)
+
+setwd("~/git/r-fts")
+
+single_or_empty_fields = c(
+  "id"
+  ,"amountUSD"
+  ,"budgetYear"
+  ,"conributionType"
+  ,"createdAt"
+  ,"date"
+  ,"decisionDate"
+  ,"description"
+  ,"grandBargainEarmarkingType"
+  ,"exchangeRate"
+  ,"firstReportedDate"
+  ,"flowType"
+  ,"newMoney"
+  ,"originalAmount"
+  ,"originalCurrency"
+  ,"method"
+  ,"parentFlowId"
+  ,"status"
+  ,"updatedAt"
+  ,"versionId"
+  ,"boundary"
+  ,"onBoundary"
+  ,"refCode"
+)
+
+object_fields = c(
+  "source"="sourceObjects",
+  "destination"="destinationObjects"
+)
+
+object_field_cols = c("type","id","name")
+
+other_multi_fields = c("reportDetails", "keywords") # TODO: implement if necessary
+
+flatten_flow = function(single_flow){
+  flat_flow = data.frame(t(unlist(single_flow[single_or_empty_fields])),stringsAsFactors=F)
+  
+  for(i in 1:length(object_fields)){
+    field_list = single_flow[object_fields[i]][[1]]
+    field_df = data.table(t(sapply(field_list,`[`,object_field_cols)))
+    field_df$type = sapply(field_df$type,`[`,1)
+    field_df_grouped = field_df[,.(id=paste(id,collapse=" | "),name=paste(name,collapse=" | ")),by=.(type)]
+    field_prefix = names(object_fields)[i]
+    flat_flow[,paste(field_prefix,field_df_grouped$type,"id",sep="_")] = field_df_grouped$id
+    flat_flow[,paste(field_prefix,field_df_grouped$type,"name",sep="_")] = field_df_grouped$name
+  }
+  
+  return(flat_flow)
+}
+
+fts.flow = function(boundary=NULL,auth=NULL){
+  page = 1
+  base.url = paste0("https://api.hpc.tools/v1/public/fts/flow?",boundary)
+  if(is.null(boundary)){
+    stop("Boundary is a required field.")
+  }
+  message("Fetching page ",page," for boundary ", boundary)
+  if(!is.null(auth)){
+    res = GET(
+      base.url
+      ,authenticate(auth$user, auth$pass)
+    )
+  }else{
+    res = GET(base.url)
+  }
+  if(res$status_code==200){
+    dat = content(res)
+    data_list = list()
+    flows = dat$data$flows
+    flow_df = rbindlist(lapply(flows,flatten_flow),fill=T)
+    data_list[[page]] = flow_df
+    while("nextLink" %in% names(dat$meta)){
+      page = page + 1
+      message("Fetching page ",page," for boundary ", boundary)
+      if(!is.null(auth)){
+        res = GET(
+          dat$meta$nextLink
+          ,authenticate(auth$user, auth$pass)
+        )
+      }else{
+        res = GET(dat$meta$nextLink)
+      }
+      dat = content(res)
+      flows = dat$data$flows
+      flow_df = rbindlist(lapply(flows,flatten_flow),fill=T)
+      data_list[[page]] = flow_df
+    }
+    return(rbindlist(data_list,fill=T))
+  }else{
+    stop("HTTP error: ",res$status_code)
+  }
+}
+auth=NULL # Fill this in, but don't save it
+for(i in 2000:2022){
+  b = paste0("year=",i)
+  tmp_flows = fts.flow(boundary=b,auth=auth)
+  fname = paste0("all_flows/fts",i,".csv")
+  fwrite(tmp_flows,fname)
+}
